2a72ff6f1c69740cdb0c1dd1dc76918ac235edd5
William Morocho <williammorocho@coopdaquilema.com>
2018-10-18 07:35:52 -0500

    Trabajos en el fronend softmant

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//wmorocho importado 
+import{FormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http'
+//fin importado
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
